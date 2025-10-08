881b7b04d07ae4779064197198e10d8d1acdf1ee
King-Ephraim-101 <ephraimmoye@gmail.com>
2025-10-08 22:14:48 +0200

    Add files via upload

---
+++ Week 8 Assignment Python Frameworks.py
@@ -0,0 +1,7 @@
+Traceback (most recent call last):
+  File "main.py", line 2, in <module>
+    import streamlit as st
+ModuleNotFoundError: No module named 'streamlit'
+
+
+** Process exited - Return Code: 1 **
