def1ac092ae785c9befb0115d1143f935cbc4507
King-Ephraim-101 <ephraimmoye@gmail.com>
2025-10-08 22:08:02 +0200

    Add files via upload

---
+++ Week 7 Assignment Analyzing Data with Pandas and Visualizing Results with Matplotlib.py
@@ -0,0 +1,7 @@
+Traceback (most recent call last):
+  File "main.py", line 4, in <module>
+    import matplotlib.pyplot as plt
+ModuleNotFoundError: No module named 'matplotlib'
+
+
+** Process exited - Return Code: 1 **
