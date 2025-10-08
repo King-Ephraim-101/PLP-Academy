81c8f98b7bbb6fc6ca9a21569b76512992aea595
King-Ephraim-101 <ephraimmoye@gmail.com>
2025-10-08 15:11:15 +0200

    Add files via upload

---
+++ Week 5 Assignment Inheritance And Polymorphism.py
@@ -0,0 +1,13 @@
+Assignment 1
+Apple iPhone 15 with battery life of 20h
+Charged for 5 hours. Battery life is now 25h
+Asus ROG Phone 7 with battery life of 15h
+Played games for 3 hours. Battery life now 9h
+
+Activity 2
+Driving 🚗
+Flying ✈️
+Sailing ⛴️
+
+
+** Process exited - Return Code: 0 **
